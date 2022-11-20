a40dad156f84a5a16b42d188758e6dd285b1a69e
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-11-20 17:36:58 -0300

    Clase Partido: contructor, getters y setters

---
+++ TpSegundaParteMundial/src/Partido.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+
+public class Partido {
+
+    private Date fecha;
+    private Equipo local;
+    private Equipo visitante;
+    private Resultado resultado;
+
+
+    //constructor
+    public Partido(Date fecha, Equipo local, Equipo visitante, Resultado resultado) {
+        this.fecha = fecha;
+        this.local = local;
+        this.visitante = visitante;
+        this.resultado = resultado;
+    }
+
+    //getters y setters
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Equipo getLocal() {
+        return local;
+    }
+
+    public void setLocal(Equipo local) {
+        this.local = local;
+    }
+
+    public Equipo getVisitante() {
+        return visitante;
+    }
+
+    public void setVisitante(Equipo visitante) {
+        this.visitante = visitante;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Resultado resultado) {
+        this.resultado = resultado;
+    }
+
+
+
+}
