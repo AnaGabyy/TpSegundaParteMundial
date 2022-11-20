cc5f26d9ceff4a415e05ccb09f94250626e4d231
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-11-20 20:10:27 -0300

    Clase EtapaMundial: constructor, getters, setters y addPartido

---
+++ TpSegundaParteMundial/src/EtapaMundial.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList<Partido>partidos;
+    private ArrayList<Equipo>equiposQueAvanzan;
+
+    public EtapaMundial(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+        this.partidos = partidos;
+        this.equiposQueAvanzan = equiposQueAvanzan;
+    }
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void setDescripcionEtapa(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return equiposQueAvanzan;
+    }
+
+    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
+        this.equiposQueAvanzan = equiposQueAvanzan;
+    }
+
+    public void addPartido(Partido partido){
+        this.partidos.add(partido);
+    }
+
+
+
+}
