35d47709eac4ae3b103117c05e84b9f6351aec4f
Ana Medina <112995260+AnaGabyy@users.noreply.github.com>
2022-11-28 12:44:01 -0300

    metodos

--- TpSegundaParteMundial/src/Grupo.java
+++ TpSegundaParteMundial/src/Grupo.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class Grupo extends EtapaMundial{
+
+    private ArrayList<Equipo> equiposQueAvanzan;
 
     public Grupo(String descripcionEtapa) {
         super(descripcionEtapa);
+    }
+
+    @Override
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return equiposQueAvanzan;
+    }
+
+    @Override
+    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
+        this.equiposQueAvanzan = equiposQueAvanzan;
+    }
+
+    @Override
+    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan){
+        this.equiposQueAvanzan.add(equiposQueAvanzan);
     }
 }
