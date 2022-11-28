3944aa7b1840e49b45590ad5dc901420046256bd
Ana Medina <112995260+AnaGabyy@users.noreply.github.com>
2022-11-28 12:43:50 -0300

    metodos

--- TpSegundaParteMundial/src/Llave.java
+++ TpSegundaParteMundial/src/Llave.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class Llave extends EtapaMundial{
+
+    private ArrayList<Equipo> equiposQueAvanzan;
 
     public Llave(String descripcionEtapa) {
         super(descripcionEtapa);
+    }
+
+    @Override
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return equiposQueAvanzan;
+    }
+
+    @Override
+    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
+        this.equiposQueAvanzan = equiposQueAvanzan;
+    }
+
+    @Override
+    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan){
+        this.equiposQueAvanzan.add(equiposQueAvanzan);
     }
 }
