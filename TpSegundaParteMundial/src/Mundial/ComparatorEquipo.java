cad390a99bfad99cacac6bc56fe5ea9d993990f6
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-12-07 19:07:16 -0300

    Nueva clase ComparatorEquipo para ordenar ArrayList

---
+++ TpSegundaParteMundial/src/Mundial/ComparatorEquipo.java
@@ -0,0 +1,24 @@
+package Mundial;
+
+import java.util.Comparator;
+
+public class ComparatorEquipo implements Comparator<Equipo> {
+
+    @Override
+    public int compare(Equipo equipo1, Equipo equipo2) {
+        //Si el 1er objeto es mayor al 2do me retorna un positivo
+        //Si el 2do es mayor al 1ero retorna un negativo
+        //Si son iguales retorna 0
+        int respuesta = 0;
+        if (equipo1.getPuntaje() > equipo2.getPuntaje()){
+            respuesta = 1;
+        } else if (equipo1.getPuntaje() < equipo2.getPuntaje()) {
+            respuesta = -1;
+
+        }else {
+            respuesta = 0;
+        }
+
+        return respuesta;
+    }
+}
