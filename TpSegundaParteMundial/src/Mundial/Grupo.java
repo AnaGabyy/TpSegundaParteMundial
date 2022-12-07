26db1b6fda0d1e6afdd1ea8b550f1285560a5824
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-12-07 05:07:30 -0300

    Eliminé el for del metodo getEquiposQueAvanzan

--- TpSegundaParteMundial/src/Mundial/Grupo.java
+++ TpSegundaParteMundial/src/Mundial/Grupo.java
@@ -4,53 +4,30 @@ import java.util.ArrayList;
 
 public class Grupo extends EtapaMundial{
 
-    private ArrayList<Partido>grupos;
-    private int puntaje;
+    private ArrayList<Equipo>equiposDelGrupo = new ArrayList<>();
     private ArrayList<Equipo>equiposQueAvanzan;
 
-    public Grupo(String descripcionEtapa) {
-        super(descripcionEtapa);
-        this.grupos = grupos;
-        this.puntaje = 0;
+
+    public Grupo(String descripcionEtapa, ArrayList<Partido> partidos) {
+        super(descripcionEtapa, partidos);
         this.equiposQueAvanzan = equiposQueAvanzan;
     }
 
+
     //Getters y setters
-    public ArrayList<Partido> getGrupos() {
-        return grupos;
+
+    public void setEquipoGrupo(Equipo equipo){
+
+        this.equiposDelGrupo.add(equipo);
     }
 
-    public void setGrupos(ArrayList<Partido> grupos) {
-        grupos = grupos;
-    }
+    public ArrayList<Equipo> getEquipoDelGrupo(){
 
-    public int getPuntaje() {
-        return puntaje;
-    }
-
-    public void setPuntaje(int puntaje) {
-        this.puntaje = puntaje;
+        return equiposDelGrupo;
     }
 
     @Override
     public ArrayList<Equipo> getEquiposQueAvanzan() {
-
-        for (int i = 0; i < getPartidos().size(); i++) {
-            if (getPartidos().get(i).getResultado().ganoLocal()){
-
-                equiposQueAvanzan.add(getPartidos().get(i).getLocal());
-                setPuntaje(3);
-
-            } else if (getPartidos().get(i).getResultado().empate()) {
-                equiposQueAvanzan.add(getPartidos().get(i).getLocal());
-                setPuntaje(1);
-
-            } else {
-                equiposQueAvanzan.add(getPartidos().get(i).getVisitante());
-                setPuntaje(3);
-            }
-        }
-
         return equiposQueAvanzan;
     }
 
@@ -63,7 +40,6 @@ public class Grupo extends EtapaMundial{
     public void addEquiposQueAvanzan(Equipo equiposQueAvanzan){
         this.equiposQueAvanzan.add(equiposQueAvanzan);
     }
-
 
 
 }
