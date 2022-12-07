b29a9f815bc64ed4a541c7bb1efe4e1a0136e268
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-12-07 05:09:09 -0300

    Metodos de puntaje

--- TpSegundaParteMundial/src/Mundial/Equipo.java
+++ TpSegundaParteMundial/src/Mundial/Equipo.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 public class Equipo {
 
     private String nombre;
+    private int puntaje;
     private ArrayList<Partido>partidosJugados;
 
     public Equipo(String nombre) {
         this.nombre = nombre;
+        this.puntaje = puntaje;
         this.partidosJugados = partidosJugados;
+
     }
 
     public String getNombre() {
@@ -29,5 +32,18 @@ public class Equipo {
 
     public void addPartidosJugados(Partido partidosJugados){
         this.partidosJugados.add(partidosJugados);
+    }
+
+    public void setPuntaje(int x){
+       this.puntaje = sumaPuntaje(x);
+
+    }
+
+    public int getPuntaje(){
+        return this.puntaje;
+    }
+
+    private int sumaPuntaje(int x){
+        return this.puntaje + x;
     }
 }
