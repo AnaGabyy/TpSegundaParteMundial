4440f26e233da702204b26f7ceef83da52bf83a8
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-12-08 01:31:23 -0300

    cambios

--- TpSegundaParteMundial/src/Mundial/Grupo.java
+++ TpSegundaParteMundial/src/Mundial/Grupo.java
@@ -5,24 +5,18 @@ import java.util.ArrayList;
 public class Grupo extends EtapaMundial{
 
     private ArrayList<Equipo>equiposDelGrupo = new ArrayList<>();
-    private ArrayList<Equipo>equiposQueAvanzan;
-
+    private ArrayList<Equipo>equiposQueAvanzan = new ArrayList<>();
 
     public Grupo(String descripcionEtapa, ArrayList<Partido> partidos) {
         super(descripcionEtapa, partidos);
         this.equiposQueAvanzan = equiposQueAvanzan;
     }
 
-
-    //Getters y setters
-
     public void setEquipoGrupo(Equipo equipo){
-
         this.equiposDelGrupo.add(equipo);
     }
 
     public ArrayList<Equipo> getEquipoDelGrupo(){
-
         return equiposDelGrupo;
     }
 
