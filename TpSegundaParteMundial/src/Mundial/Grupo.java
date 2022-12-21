37395487efe6eafc0a2da9942533c9bea2e85ccb
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2022-12-21 18:16:35 -0300

    Añadí el super.toString

--- TpSegundaParteMundial/src/Mundial/Grupo.java
+++ TpSegundaParteMundial/src/Mundial/Grupo.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 public class Grupo extends EtapaMundial{
 
     private ArrayList<Equipo>equiposDelGrupo = new ArrayList<>();
-    private ArrayList<Equipo>equiposQueAvanzan = new ArrayList<>();
 
     public Grupo(String descripcionEtapa, ArrayList<Partido> partidos) {
         super(descripcionEtapa, partidos);
-        this.equiposQueAvanzan = equiposQueAvanzan;
     }
 
     public void setEquipoGrupo(Equipo equipo){
@@ -21,25 +19,14 @@ public class Grupo extends EtapaMundial{
     }
 
     @Override
-    public ArrayList<Equipo> getEquiposQueAvanzan() {
-        return equiposQueAvanzan;
-    }
-
-    @Override
-    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
-        this.equiposQueAvanzan = equiposQueAvanzan;
-    }
-
-    @Override
-    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan){
-        this.equiposQueAvanzan.add(equiposQueAvanzan);
+    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan) {
+        this.getEquiposQueAvanzan().add(equiposQueAvanzan);
     }
 
     @Override
     public String toString() {
-        return "Grupo{" +
+        return super.toString() +"Grupo[" +
                 "equiposDelGrupo=" + equiposDelGrupo +
-                ", equiposQueAvanzan=" + equiposQueAvanzan +
-                '}';
+                ']';
     }
 }
--- TpSegundaParteMundial/src/Mundial/Llave.java
+++ TpSegundaParteMundial/src/Mundial/Llave.java
@@ -4,43 +4,17 @@ import java.util.ArrayList;
 
 public class Llave extends EtapaMundial{
 
-    private ArrayList<Partido>llaves;
-    private ArrayList<Equipo>equiposQueAvanzan;
-
-    public Llave(String descripcionEtapa, ArrayList<Partido> partidos, ArrayList<Partido> llaves) {
+    public Llave(String descripcionEtapa, ArrayList<Partido> partidos) {
         super(descripcionEtapa, partidos);
-        this.llaves = llaves;
-        this.equiposQueAvanzan = equiposQueAvanzan;
-    }
-
-    public ArrayList<Partido> getLlaves() {
-        return llaves;
-    }
-
-    public void setLlaves(ArrayList<Partido> llaves) {
-        this.llaves = llaves;
     }
 
     @Override
-    public ArrayList<Equipo> getEquiposQueAvanzan() {
-        return equiposQueAvanzan;
-    }
-
-    @Override
-    public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
-        this.equiposQueAvanzan = equiposQueAvanzan;
-    }
-
-    @Override
-    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan){
-        this.equiposQueAvanzan.add(equiposQueAvanzan);
+    public void addEquiposQueAvanzan(Equipo equiposQueAvanzan) {
+        this.getEquiposQueAvanzan().add(equiposQueAvanzan);
     }
 
     @Override
     public String toString() {
-        return "Llave{" +
-                "llaves=" + llaves +
-                ", equiposQueAvanzan=" + equiposQueAvanzan +
-                '}';
+        return super.toString();
     }
 }
